56e657d7c1de0d523c27e62268dd1b1dc96a9e1b
Cooltor <romaindubus86@gmail.com>
2022-10-12 10:31:45 +0200

    commit2

--- training.php
+++ training.php
@@ -1,9 +1,11 @@
 <style>
     h2{
-        margin: 0;
-        font-size: 15px;
-        color: red;
+        margin-bottom: 5px;
+        font-size: 30px;
+        color: green;
         text-align: center;
+        border: solid 3px blue;
+        border-radius: 3px;
     }
 </style>
 
@@ -15,7 +17,7 @@ echo 'Bonjour tout le monde'; // echo est une instruction d'affichage
 echo '<h3> Hello... </h3>'; // echo interprète le html
 print 'Salut'; // print fait la même chose que echo
 
-echo '<hr> <h2> Les commentaires </h2>';
+echo ' <h2> Les commentaires </h2>';
 
 ?>
 
@@ -34,7 +36,7 @@ echo 'Commentaire sur plusieurs ligne<br>'; /* Ceci est un commentaire
 
 echo 'commentaire'; #ceci est un commentaire
 
-echo '<hr><h2>Les variables / Types / Déclaration / Affectation</h2>'; // Une variable est un espace nommé permettant de stocker une valeur
+echo '<h2>Les variables / Types / Déclaration / Affectation</h2>'; // Une variable est un espace nommé permettant de stocker une valeur
 
 $a = 10;
 echo gettype($a) . '<br>'; // gettype() est une fonction prédéfinie permettant de connaitre le type d'une variable : INTEGER
@@ -50,6 +52,54 @@ echo gettype($d) . '<br>';
 
 $e = true;
 echo gettype($e) . '<br>';
+
+echo '<h2>Assignation par référence</h2>';
+
+$f = 'Salut';
+$g = &$f; // Ceci est ass. par référence
+echo $f . '<br>';
+echo $g . '<br>';
+
+
+echo '<h2>La concaténation</h2>';
+
+$x = 'Bonjour';
+$y = 'Bobby';
+
+echo $x . ' ' . $y . '<br>'; // Le . permet de faire la concaténation
+
+echo $x .$y. '<br>';
+
+echo "$x $y". "<br>";
+
+echo '$x $y'. "<br>"; // avec simple quotes les variables ne sont pas évaluées 
+
+$prenom1= "Bruno";
+$prenom1= "Marie";
+
+echo $prenom1 . "<br>"; // La valeur venant après écrase celle d'avant
+
+$prenom2 = 'Nicolas'; // Affectation de Nicolas à  $prenom2
+$prenom2 .='Romain'; // Rajoute Romain ds $prenom2 sans écraser l'ancienne valeur
+
+echo $prenom2. "<br>";
+
+
+echo '<h2>Les constantes</h2>'; // Une constante est une variable qui ne change pas de valeur
+
+define('TVA', 0.2);
+define('VILLE', 'Paris'); 
+
+echo TVA . "<br>";
+echo VILLE . "<br>";
+
+// Les constantes magiques
+
+echo __DIR__ . "<br>"; // Donne le dossier ds lequel je me trouve
+
+echo __FILE__ . "<br>"; // Donne le fichier courant
+
+echo __LINE__ . "<br>"; // Donne la ligne
 
 
 
